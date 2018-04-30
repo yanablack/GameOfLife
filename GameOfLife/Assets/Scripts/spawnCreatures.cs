f925232f637e43b91061bd4328f178559b749da9
yanablack <yanablack@users.noreply.github.com>
2018-04-30 22:17:27 +0300

    commit.spawnRandomCreatures
    created an empy gameObject for sceneManager that has a script attached to it that all it does is to spawn random creatures at random positions in a predefined range

---
+++ GameOfLife/Assets/Scripts/spawnCreatures.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnCreatures : MonoBehaviour {
+
+    public GameObject[] creatures;
+    private float spawnInterval = 0.3f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnCreature", 1.0f, spawnInterval);
+    }
+
+    void SpawnCreature()
+    {
+        Vector3 position = new Vector3(Random.Range(-30.0F, 30.0F), 0, Random.Range(-30.0F, 30.0F));
+        Instantiate(creatures[Random.Range(0, creatures.Length)], position, transform.rotation);
+    }
+}
